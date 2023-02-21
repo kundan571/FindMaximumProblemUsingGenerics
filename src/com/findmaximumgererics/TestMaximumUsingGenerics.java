cdd3b080354bdfdf488f2afbf446ba5418d72885
kundan571 <kundanchauhan1997@gmail.com>
2023-02-21 23:01:41 +0530

    Added: Finding maximum of three INTEGER, FLOAT And STRING usingmethod generics

--- src/com/findmaximumgererics/TestMaximumUsingGenerics.java
+++ src/com/findmaximumgererics/TestMaximumUsingGenerics.java
@@ -18,16 +18,21 @@ public class TestMaximumUsingGenerics<T extends Comparable<T>> {
         if (z.compareTo(max) > 0) {
             max = z;
         }
-        System.out.println("Maximum of: " + x + ", " + y + ", " + z + " is: " + max);
+        Printmaximum(x, y, z, max);
         return max;
+    }
+
+    private static <T extends Comparable<T>> void Printmaximum(T x, T y, T z, T max) {
+        System.out.println("Maximum of: " + x + ", " + y + ", " + z + " is: " + max);
+        
     }
 
 
     public static void main(String[] args) {
         // for integer
-        //  new TestMaximumUsingGenerics<>(30,200,12);
+          new TestMaximumUsingGenerics<>(30,200,12);
         // for double
-        //  new TestMaximumUsingGenerics<>(20.5,40.1,5.2);
+          new TestMaximumUsingGenerics<>(20.5,40.1,5.2);
         // for string
         new TestMaximumUsingGenerics<>("kundan", "rahul", "shubham");
         new TestMaximumUsingGenerics<>('z','q','t');
