4798cccbd446d603cc35cb5f29641ffd44c120eb
kundan571 <kundanchauhan1997@gmail.com>
2023-02-21 14:03:56 +0530

    Added: Welcome message

---
+++ src/com/findmaximumgererics/FindMaximumUsingGenerics.java
@@ -0,0 +1,7 @@
+package com.findmaximumgererics;
+
+public class FindMaximumUsingGenerics {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Using Generics Program");
+    }
+}
